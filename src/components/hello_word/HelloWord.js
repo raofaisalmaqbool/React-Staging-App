065bd263ec7d0c32252d4692170bd00b98a7401c
faisalmaqbool <faisalmaqbool.spyresync@gmail.com>
2022-11-11 23:15:04 +0500

    make a component and set routers of project

---
+++ src/components/hello_word/HelloWord.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css';
+
+export default function HelloWord() {
+  return (
+    <div>
+    <div className='hello_word'>Hello Word</div>
+        <br></br> <br></br>
+    <div className='welcome'>Welcome To My React App</div>
+    </div>
+
+  )
+}
