c0334eeef2c3794e586c8771489ec3f823fe9511
faisalmaqbool <faisalmaqbool.spyresync@gmail.com>
2022-11-12 17:46:41 +0500

    use of useState

--- src/components/ClickEvent/ClickEvent.js
+++ src/components/ClickEvent/ClickEvent.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import './style.css';
-
+import React from "react";
+import { useState } from "react";
+import "./style.css";
 
 export default function ClickEvent() {
+  let data0 = "Click Event";
+  const [data, setData] = useState(data0);
 
-    let data = 'Click Event';
-    
-    function OnClick(){
-        data = 'On Click Function Called Successfully.';
-        alert(data);
-    }
+  // function OnClick(){
+  //     data = 'On Click Function Called Successfully.';
+  //     alert(data);
+  // }
+
+  function OnClick() {
+    let data1 = "On Click Function Called Successfully.";
+    setData(data1);
+    // alert(data1);
+  }
 
   return (
-    <div className='ClickEvent'>
-        <h1>{data}</h1>
-        <br/><br/>
-        <button onClick={OnClick}>Click Me</button>
+    <div className="ClickEvent">
+      <h1>{data}</h1>
+      <br />
+      <br />
+      <button onClick={OnClick}>Click Me</button>
     </div>
-  )
+  );
 }
