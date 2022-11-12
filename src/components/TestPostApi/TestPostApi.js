57c9aab2fd31b52fec2ec79ebc6f50502d535db3
faisalmaqbool <faisalmaqbool.spyresync@gmail.com>
2022-11-12 12:25:37 +0500

    post api with body and heandel success and error

--- src/components/TestPostApi/TestPostApi.js
+++ src/components/TestPostApi/TestPostApi.js
@@ -1,37 +1,50 @@
-// import React from 'react'
 import React, { useState, useEffect } from 'react'
 import './style.css';
 
 
 export default function TestPostApi() {
+    const [email, setStateEmail] = useState('');
+    const [password, setStatePassword] = useState('');
+
+    function userLogin(){
+        console.warn(email, password);
+        let data = {email, password}
+
+        fetch("http://52.198.220.115/api/user_login/", {
+            method: 'POST',
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+
+    }).then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    }
+
+
   return (
     <div className='TestPostApi'>
 
         <h1>Test Post Api</h1>
 
-        {/* <div class="container">
 
-            <label for="email"><b>Email: </b></label>
-            <input type="text" placeholder="Enter Your Email" name="email" required /> <br/>
-
-            <label for="password"><b>Password: </b></label>
-            <input type="password" placeholder="Enter Your Password" name="password" required /><br/>
-
-            <button type="submit">Login</button>
-
-        </div> */}
-        <div class="imgcontainer">
-            <img id='img123' src="img_avatar2.png" alt="Avatar" class="avatar" />
+        <div className="imgcontainer">
+            <img id='img123' src="img_avatar2.png" alt="Avatar" className="avatar" />
         </div>
 
-        <div class="container">
+        <div className="container">
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Your Email" name="email" required /> 
+            <input type="text" placeholder="Enter Your Email" name="email" value={email} onChange={(e)=>{setStateEmail(e.target.value)}} required /> 
 
             <label for="password"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="password" required />
+            <input type="password" placeholder="Enter Password" name="password" value={password} onChange={(e)=>{setStatePassword(e.target.value)}} required />
                 
-            <button type="submit">Login</button>
+            <button type="submit" onClick={userLogin}>Login</button>
 
         </div>
 
